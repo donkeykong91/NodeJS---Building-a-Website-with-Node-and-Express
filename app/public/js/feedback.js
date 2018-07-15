d888aacab3f330d508aa8caa78958f498bd3ec08
donkeykong91 <darylnator@gmail.com>
2018-07-15 04:03:35 -0700

    Added feedback.js to modify the data that will be viewed in the form page

---
+++ app/public/js/feedback.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    $getJSON("api", updateFeedback);
+
+
+    function updateFeedback (data) {
+        
+        var output = ""    ;
+
+        $.each(data, function (key, item) {
+
+            output += '            <div class="feedback-item item-list media-list">';
+
+            output += '                <div class="feedback-item media">';
+
+            output += '                    <div class="feedback-info media-body">';
+
+            output += '                        <div class="feedback-head">';
+
+            output += '                            <div class="feedback-title">' + item.title;
+                                        
+            output += '                                <small class="feedback-name label label-info">';
+
+            output += '                                </small>';
+
+            output += '                            </div>';
+
+            output += '                        </div>';
+
+            output += '                        <div class="feedback-message">' + item.message;
+                                    
+            output += '                        </div>';
+
+            output += '                    </div>';
+
+            output += '                </div>';
+
+            output += '            </div>';
+
+        });
+
+        $(".feedback-messages").html(output);
+
+    }
+
+});
