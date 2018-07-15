0578aadec79f16d62e500266ade8425cb5fd4deb
donkeykong91 <darylnator@gmail.com>
2018-07-15 05:54:12 -0700

    Added post route for api to display content of feedback on that page

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,10 +2,27 @@ var express = require("express");
 
 var router = express.Router();
 
+var bodyParser = require("body-parser");
+
 var feedbackData = require("../data/feedback.json");
+
 
 router.get("/api", function (req, res) {
     
+    res.json(feedbackData);
+
+});
+
+
+router.use(bodyParser.json());
+
+router.use(bodyParser.urlencoded({ extended: false }));
+
+
+router.post("/api", function (req, res) {
+
+    feedbackData.unshift(req.body);
+
     res.json(feedbackData);
 
 });
